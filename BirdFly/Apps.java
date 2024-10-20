fbc802c2aff94859dfc36c93da10460e8436e552
Traveliki- <https://github.com/duriskifeb>
2024-10-20 19:29:44 +0700

    semangat cuyy

---
+++ BirdFly/Apps.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+public class Apps {
+    public static void main(String[] args) {
+        int boardWith = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setSize(boardWith, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        F
+    }
+}
---
+++ BirdFly/MainMenu.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+
+
+public class MainMenu {
+   public static void main(String[] args) {
+    System.out.println("Welcome");
+   } 
+}
